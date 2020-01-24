e6653f6335ca28e4fce6c94a6495ede5f427f3d4
Ferdy Macleod <ferdymacleod21@gmail.com>
2020-01-24 10:10:54 +1000

    litera lly just removed one empty line

--- helpers/csvHelper.js
+++ helpers/csvHelper.js
@@ -1,9 +1,8 @@
 const Papa = require('papaparse')
 
   function parseCsv(files) {
-      let result = Papa.parse(files)
-      return console.log(result)
-
+    let result = Papa.parse(files)
+    return console.log(result)
 }
 
 module.exports = parseCsv
