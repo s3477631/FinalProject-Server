2475f3033052f33738f91fdcd81f721ae3db3708
Devin <devinwreeks@gmail.com>
2020-01-29 12:41:25 +1000

    fixed cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(morgan("combined"));
 
 app.use(
     cors({
-      origin: "https://boiling-inlet-28252.herokuapp.com"
+      origin: "https://boiling-inlet-28252.herokuapp.com/auth/register"
     })
   );
 app.use(require("./routes"));
