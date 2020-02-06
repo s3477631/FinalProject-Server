3e4f31467b6021b02da48d2841a31699d493666d
Devin <devinwreeks@gmail.com>
2020-02-06 13:17:30 +1000

    updated cors issue

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(morgan("combined"));
 
 app.use(
     cors({
-      origin: "https://boiling-inlet-28252.herokuapp.com/"
+      origin: "https://goofy-cori-5c2197.netlify.com/"
     })
   );
 app.use(require("./routes"));
